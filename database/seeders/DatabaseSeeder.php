117a54f5c1eb1f5adece112ec579d2c9451d55c9
Frank PC <pikino84@gmail.com>
2026-03-01 13:29:20 -0500

    Include PlansAndFeaturesSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+            PlansAndFeaturesSeeder::class,
+        ]);
     }
 }
